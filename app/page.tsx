a9b3e5644a28df00e35be0bf3b36181b0e10273e
Johan Villamil <johinsdev@gmail.com>
2024-05-17 17:30:04 -0500

    v1 diario diverso

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Diario diverso</h1>
+      <h1>Diario diverso v1</h1>
     </main>
   );
 }
