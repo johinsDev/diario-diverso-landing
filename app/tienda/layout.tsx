39e4909d89eecf2c17d46796f7b4a7a6895557af
Johan Villamil <johinsdev@gmail.com>
2024-06-18 18:37:43 -0500

    fix mobile image

--- app/tienda/layout.tsx
+++ app/tienda/layout.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 
 export default function StoreLayout() {
-  return <div className="bg-[#FCF6DA] flex items-center flex-1 min-h-[50rem] lg:min-h-fit">
-    <div className="relative mx-auto container h-full lg:h-min lg:aspect-video">
+  return <div className="bg-[#FCF6DA] flex justify-center flex-1 flex-col min-h-[50rem] lg:min-h-fit">
+    <div className="relative mx-auto container flex-1 lg:h-min lg:aspect-video">
       <Image src="/store.png" alt="Store" layout="fill" className="object-fill hidden lg:block" />
 
       <Image src="/store-mobile.png" alt="Store" layout="fill" className="object-fill md:object-contain lg:hidden" />
