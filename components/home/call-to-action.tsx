66507596bc331ad8cf6e21fcc96f4bb44e5b9dc8
Johan Villamil <johinsdev@gmail.com>
2024-06-17 19:52:44 -0500

    refactor: Update CallToAction component with new text and button label

--- components/home/call-to-action.tsx
+++ components/home/call-to-action.tsx
@@ -2,25 +2,26 @@ import Image from "next/image";
 import { Button } from "../ui/button";
 
 export function CallToAction() {
-  return <section className="relative bg-white pb-24 overflow-hidden full-width">
+  return <section className="relative bg-white py-20 overflow-hidden full-width">
     <div className="bg-background w-screen top-1/2 bottom-0 absolute" />
 
     <div className="container mx-auto">
-      <div className="relative bg-secondary rounded-lg px-16 py-12 text-left overflow-hidden">
-        <div className="w-1/2">
-          <h2 className="text-h2 leading-h2 text-white font-semibold">
-            Ready to transform
-            your digital presence?
+      <div className="relative bg-accent/20 rounded-lg px-4 md:px-16 py-12 text-left overflow-hidden flex flex-col">
+        <div className="w-full md:w-3/5 lg:w-1/2">
+          <h2 className="text-h2 leading-h2 text-foreground font-semibold">
+            todavia pensandolo?
           </h2>
 
-          <div className="text-gray-200 mt-4 text-xl">
-            Let us transform your digital business in just a month.
+          <div className="text-foreground/70 mt-4 text-xl">
+            Inspírate, crea y comparte tu viaje con nosotros. ¡Comienza hoy y transforma tu vida!
           </div>
 
-          <Button className="mt-8 px-12 font-semibold" variant={"black"}>Get Started</Button>
+          <Button className="mt-8 px-12 font-semibold" variant={"black"}>Adquirir mi diario</Button>
         </div>
 
-        <Image src="/hero.svg" width={512} height={512} alt="CTA" className="absolute right-0 top-0 w-2/3" />
+        <div className="absolute top-0 right-0 bottom-0 hidden md:block md:w-2/5 lg:w-1/3">
+          <Image src="/cta3.jpg" className="object-cover" alt="Hero Image" fill />
+        </div>
       </div>
     </div>
   </section>
