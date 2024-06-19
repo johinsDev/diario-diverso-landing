2d7a4634723a77cd4e6940667ab8aa8879738870
Johan Villamil <johinsdev@gmail.com>
2024-06-19 00:34:40 -0500

    refactor: Implement lazy loading for images in ListOfProducts component and remove unused code

--- app/tienda/[...slug]/page.tsx
+++ app/tienda/[...slug]/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
   Carousel,
   CarouselContent,
@@ -8,14 +10,79 @@ import {
 } from "@/components/ui/carousel";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Page() {
+  const [selectedImage, setSelectedImage] = useState('/products/kit-plenitud.png');
+
+
+
   return (
     <main className="flex-1 flex flex-col container py-4 md:py-20">
       <h1 className="font-semibold">Tienda</h1>
 
       <div className="mt-2 text-muted-foreground text-xl">
         Consulta nuestra colección completa de productos.
+      </div>
+
+      <div className="grid grid-cols-4 grid-rows-4 w-2/5 gap-2 gap-x-4">
+        <button className="bg-white aspect-[4/5] rounded-md border border-primary/40 hover:ring-2 hover:ring-accent ring-offset-2 py-1 overflow-hidden"
+          // onHover={() => setSelectedImage('/products/kit-plenitud.png')}
+          onMouseEnter={() => {
+            setSelectedImage('/products/kit-plenitud.png')
+          }}
+        >
+
+          <Image
+            src="/products/kit-plenitud.png"
+            alt="Kit Plenitud"
+            className="object-contain rounded-md h-full transform hover:scale-105 transition-all duration-300"
+            width={300}
+            height={220}
+          />
+        </button>
+        <button className="bg-white aspect-[4/5] rounded-md col-start-1 row-start-2  border border-primary/40 hover:ring-2 hover:ring-accent ring-offset-2 py-1 overflow-hidden"
+          onMouseEnter={() => setSelectedImage('/products/amigos-verde-1.png')}
+        >
+          <Image
+            src="/products/amigos-verde-1.png"
+            alt="Kit Plenitud"
+            className="object-contain rounded-md h-full transform hover:scale-105 transition-all duration-300"
+            width={300}
+            height={220}
+          />
+        </button>
+        <button className="bg-white aspect-[4/5] rounded-md col-start-1 row-start-3 border-2 border-primary/40 hover:ring-2 hover:ring-accent ring-offset-2 py-1 overflow-hidden"
+          onMouseEnter={() => setSelectedImage('/products/habitos-azul-11.png')}
+        >
+          <Image
+            src="/products/habitos-azul-11.png"
+            alt="Kit Plenitud"
+            className="object-contain rounded-md h-full transform hover:scale-105 transition-all duration-300"
+            width={300}
+            height={220}
+          />
+        </button>
+        <button className="bg-white aspect-[4/5] rounded-md col-start-1 row-start-4 border-2 border-primary/40 hover:ring-2 hover:ring-accent ring-offset-2 py-1 overflow-hidden"
+          onMouseEnter={() => setSelectedImage('/products/habitos-verde-3.png')}
+        >
+          <Image
+            src="/products/habitos-verde-3.png"
+            alt="Kit Plenitud"
+            className="object-contain rounded-md h-full transform hover:scale-105 transition-all duration-300"
+            width={300}
+            height={220}
+          />
+        </button>
+        <button className="bg-white relative rounded-md col-span-3 row-span-4">
+          <Image
+            src={selectedImage}
+            alt="Kit Plenitud"
+            className="object-contain rounded-md"
+            width={500}
+            height={700}
+          />
+        </button>
       </div>
 
       <Dialog>
