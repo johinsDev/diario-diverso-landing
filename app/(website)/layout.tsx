90a325fc1e8d11b5626fbb48ef9a0ce169fc3b6d
Johan Villamil <johinsdev@gmail.com>
2024-09-02 16:00:26 -0500

    refactor: Add lazy loading for Facebook Pixel script in layout.tsx

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { FloatingWhatsapp } from "@/components/shared/floating-whatsapp";
 import { Footer } from "@/components/shared/footer";
 import { Nav } from "@/components/shared/nav";
@@ -7,6 +8,7 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Montserrat, Poppins } from "next/font/google";
 import localFont from "next/font/local";
+import Script from "next/script";
 import { Suspense } from "react";
 import "./globals.css";
 
@@ -91,6 +93,32 @@ export default function RootLayout({
           <Suspense>
             <Footer />
           </Suspense>
+          <Script
+            id="facebook-pixel"
+            strategy="afterInteractive"
+          >
+            {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${process.env['FACEBOOK_PIXEL_ID']}');
+          fbq('track', 'PageView');
+        `}
+          </Script>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              alt=""
+              style={{ display: "none" }}
+              src={`https://www.facebook.com/tr?id=${process.env['FACEBOOK_PIXEL_ID']}&ev=PageView&noscript=1`}
+            />
+          </noscript>
         </body>
       </CSPostHogProvider>
     </html>
